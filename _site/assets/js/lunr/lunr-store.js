d7b423178e9c16a2c56f8f41159d3d080edf5a7b
GaryTheStTrooper <GaryTheStTrooper@users.noreply.github.com>
2020-04-12 05:54:58 -0500

    no site

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Portable Experimental Economics Lab",
-        "excerpt":"About the ProjectAuthor: Marco GutierrezFound my repo here.The main purpose of this repo is to make public some of the basic software tools in order to set up a lab in experimental economics.Here you will be able to find some files neccesary to: Literature to understand the basics of lab...","categories": [],
-        "tags": ["experimental economics","behavioral economics","python","ubuntu"],
-        "url": "http://localhost:4000/peel/",
-        "teaser":null}]
